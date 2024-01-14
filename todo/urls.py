e7e3a9e428f9bfb206b122beb86fc98f2d6b5281
jveer634 <jaysparx9@gmail.com>
2024-01-15 00:34:37 +0530

    add:  detail view

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoList
+from .views import TodoList, TodoDetail
 
 app_name="todo"
 
 urlpatterns = [
-    path("", TodoList.as_view(), name="list")
+    path("", TodoList.as_view(), name="list"),
+    path("<int:id>/", TodoDetail.as_view(), name="detail"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 from .models import Todo
@@ -6,3 +6,9 @@ from .models import Todo
 class TodoList(ListView):
     model = Todo
     template_name="todo_list.html"
+
+
+class TodoDetail(DetailView):
+    model = Todo
+    pk_url_kwarg="id"
+    template_name="todo_detail.html"
