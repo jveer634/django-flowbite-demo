16dacd454b7023b83221ed9fca6858c13fb6e72b
jveer634 <jaysparx9@gmail.com>
2024-01-15 00:35:22 +0530

    chore: edit todo model to add str and fields

--- todo/models.py
+++ todo/models.py
@@ -5,17 +5,20 @@ from django.urls import reverse
 
 class Todo(models.Model):
 
-    STATUS_CHOICES = [
-        ("TD", "Todo"),
-        ("IN", "In Progress"),
-        ("CD", "Completed")
-    ]
+    STATUS_CHOICES = {
+        "To Do":"To Do",
+        "In Progress": "In Progress",
+        "Completed":"Completed",
+    }
 
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
     content = models.TextField()
-    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=11, default=STATUS_CHOICES["To Do"])
+
 
     def get_absolute_url(self):
-        return reverse("todo:detail", kwargs={"pk": self.pk})
+        return reverse("todo:detail", kwargs={"id": self.id})
     
+    def __str__(self) -> str:
+        return self.title
